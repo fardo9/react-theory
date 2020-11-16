e4f181a592d45791fce9785bc21b4cd101ae352b
nazz <nazz.fardo@gmail.com>
2020-11-16 19:07:12 +0200

    finish with routing part

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, {Component} from 'react'
 import './App.scss'
-import {Route, NavLink} from 'react-router-dom'
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom'
 import About from './About/About'
 import Cars from './Cars/Cars'
+import CarDetail from "./CarDetail/CarDetail";
 
 class App extends Component {
-  render() {
 
+    state = {
+        isLoggedIn: false
+    }
+    loginHandler = () => {
+        this.setState({
+            isLoggedIn: true
+        })
+    }
+    render() {
     return (
       <div>
         <nav className="nav">
           <ul>
             <li>
-              <NavLink
-                  to='/'
-                  exact
+              <NavLink to='/' exact activeClassName={'navActive'}
               >
                   Home
               </NavLink>
@@ -26,18 +33,32 @@ class App extends Component {
               >About</NavLink>
             </li>
               <li>
-                  <NavLink to="/cars">Cars</NavLink>
+                  <NavLink to='/cars'
+                  >Cars</NavLink>
               </li>
           </ul>
         </nav>
 
         <hr/>
+        <div style={{ textAlign: 'center '}}>
+            <h3>Is logged in {this.state.isLoggedIn ? 'TRUE' : 'FALSE'}</h3>
+            <button
+                onClick={this.loginHandler}
+            >Login
+            </button>
+        </div>
 
-        <Route path="/" exact render={() => <h1>Home Page</h1>} />
-        <Route path="/about" component={About} />
-        <Route path="/cars" component={Cars}/>
+        <Switch>
+            <Route path="/" exact render={() => <h1>Home Page</h1>} />
+            { this.state.isLoggedIn ?  <Route path="/about" component={About} /> : null}
+            {/*<Route path="/about" component={About} />*/}
+            <Route path="/cars/:name" component={CarDetail}/>
+            <Route path="/cars" component={Cars}/>
 
-        {/*<About />*/}
+            {/*<Redirect from={'/about'} to={'/cars'} />*/}
+            <Route render={() => <h1 style={{color: 'red', textAlign: 'center'}}>404 not found</h1>} />
+        </Switch>
+          {/*<About />*/}
 
         {/*<Cars />/!**!/*/}
       </div>
