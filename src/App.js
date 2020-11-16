baf83b35448148adcf9e7c33c4f6a289f4de5912
nazz <nazz.fardo@gmail.com>
2020-11-16 16:35:53 +0200

    adding route

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import './App.scss'
+import {Route, NavLink} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink
+                  to='/'
+                  exact
+              >
+                  Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                  to="/about"
+
+              >About</NavLink>
+            </li>
+              <li>
+                  <NavLink to="/cars">Cars</NavLink>
+              </li>
+          </ul>
+        </nav>
+
+        <hr/>
+
+        <Route path="/" exact render={() => <h1>Home Page</h1>} />
+        <Route path="/about" component={About} />
+        <Route path="/cars" component={Cars}/>
+
+        {/*<About />*/}
+
+        {/*<Cars />/!**!/*/}
+      </div>
+    );
+  }
+}
+
+export default App
